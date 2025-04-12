009dc1e9f9b33700b68d51b02555abd8d51454fa
sthe0016 <sthevathason@gmail.com>
2025-04-12 13:43:39 +1000

    Test: Made complete changes to layout of main.html to adjust to smaller devices

--- frontend/JavaScript/main.js
+++ frontend/JavaScript/main.js
@@ -52,28 +52,19 @@ async function showItems(){
         allItems.forEach((item) => {
             const {_id, name} = item
             const newDiv = document.createElement('div');
-            newDiv.innerHTML = `<div style = " display: flex;
-            align-items: center;
-            margin-top: 10px;">
+            newDiv.innerHTML = 
+            `
+            <div class="main-item-style">
 
-            <label style="width: 400px; 
-            height: 25px; 
-            background-color: azure; 
-            border-radius: 15px; 
-            padding: 10px; 
-            border: 2px solid black; 
-            display: flex; 
-            align-items: center;
-            font-size: large;">
+            <label class = "item-label-style">
             ${name}
-            <input style = "margin-left: auto" type = "checkbox" name = "completed" value = ${_id} />  </input>
-        </label>
-       
-        <div style="margin-left: 10px;">
-            <button class = "view" style="border-radius: 20px; ;color: white; background-color: rgb(243, 243, 243); color: black;" data-id = ${_id}> view </button>
-            <button class = "del" style="border-radius: 20px; color: white; background-color: rgb(199, 62, 62); color: black; margin-left: 10px"  data-id = ${_id}> delete</button>
-        </div>
-        </div>
+            <input type = "checkbox" name = "completed" value = ${_id} />  </input>
+            </label>
+    
+            <button class = "view-button-style" data-id = ${_id}> view </button>
+            <button class = "delete-button-style"  data-id = ${_id}> delete</button>
+
+            </div>
         `
         displayItems.appendChild(newDiv)
         }
@@ -81,42 +72,30 @@ async function showItems(){
     if (itemsExist){  // This section of html displays the option to type the name of the User buying the items and the amount of the total bill.
         const twoTextField = document.createElement('div')
         twoTextField.innerHTML = `
-                <div style = "font-size: 20px; margin-top: 100px; margin-left: 220px">
-                    <div style="display: flex; align-items: center;">
-                        <label for = "itemName" style = "width: 150px; text-align: left;"> User   </label >  
-                        <div style="display: flex; flex-direction: column; height: 20px"> 
-                            <input  type="text" id="billUser">
-                            <span id="billUserMsg" style = "color: red; font-size: 14px; display: none; margin-top: 5px">User is required to create bill</span>
-                        </div>
-                    </div>
-                
+        <div class = "input-styles">
+            <div class = "label-text-input-styles">
+                <label class = "label-styles" for = "itemName" > User:  </label >  
+                <div class = "text-input-error-div-style">
+                    <input  class = "input-text-style" type="text" id="billUser">
+                    <span id="billUserMsg"; class = "error-msg-style">Username is required to create bill</span>
                 </div>
+            </div>
 
-                <div style = "font-size: 20px; margin-top: 50px; margin-left: 220px">
-                    <div style="display: flex; align-items: center;">
-                        <label for = "itemDescription" style = "width: 150px; text-align: left;"> Bill Price: </label >
-                        <div style="display: flex; flex-direction: column; height: 20px"> 
-                            <input  type="text" id="billPrice">
-                            <span id="billPriceMsg" style = "color: red; font-size: 14px; display: none; margin-top: 5pxpx;">Please provide bill price in digits</span>
-                        </div>    
-                    </div> 
-                </div>`
+            <div class = "label-text-input-styles">
+                <label class = "label-styles" for = "itemDescription" > Bill Price: </label >
+                <div class = "text-input-error-div-style"> 
+                    <input class = "input-text-style" type="text" id="billPrice">
+                    <span id="billPriceMsg" class = "error-msg-style">Please provide bill price in digits</span>
+                </div> 
+            </div>
+        </div>
+        `
         displayItems.appendChild(twoTextField)    
         const billButton = document.createElement('div')
-            billButton.innerHTML = `<div style = "margin-top: 30px;
-            margin-left: 370px;">
-                <button type = "button" style = " background-color: rgb(227, 59, 115);
-                    border-radius: 12px;
-                    width: 120px;
-                    height: 30px; 
-                    border: white;
-                    font-size: medium;
-                    color: white;
-                    "
+            billButton.innerHTML = `<button type = "button" class = "create-bill-button-style"
                     onclick="return createBill()">
                     <b> Create bill </b>
-                </button>
-            </div>`
+                </button>`
         displayItems.appendChild(billButton)
     }
     
@@ -139,13 +118,13 @@ async function deleteItem(itemID){
 
 // all the view and delete buttons are added as children to the div section with id itemHeading. Thus, if we add an event listener to this parent element. It will apply to the child elements too 
 document.getElementById("itemHeading").addEventListener('click', async (e) => { 
-    if (e.target.classList.contains('view')){ // functionality for the edit button
+    if (e.target.classList.contains('view-button-style')){ // functionality for the edit button
         id = e.target.dataset.id  // stored in the data element of the button
         console.log(`view button pressed and data id is ${e.target.dataset.id}`)
         window.localStorage.setItem("itemID", id)
         window.location = "view.html"      
     }
-    else if (e.target.classList.contains('del')){ // functionality for the delete button
+    else if (e.target.classList.contains('delete-button-style')){ // functionality for the delete button
         id = e.target.dataset.id  // stored in the data element of the button
         console.log(`delete button pressed and data id is ${e.target.dataset.id}`)
         try{
